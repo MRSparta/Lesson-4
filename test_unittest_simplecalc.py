976d89c4250a749de228ba2b5f7a0124a6be0b90
Mahfuzur Rahman <MRahman@spartaglobal.com>
2019-11-06 15:55:32 +0000

    first commit

---
+++ test_unittest_simplecalc.py
@@ -0,0 +1,23 @@
+from simple_calc import *
+import unittest
+
+class CalcTests(unittest.TestCase):
+
+    calc = SimpleCalc()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 4), 6)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 4), 8)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4, 2), 2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
